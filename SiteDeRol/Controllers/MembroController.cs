878e095c1ed1f7dce6a2c478469e0cd05058e918
Whalisson Araujo <whalissonfelipe@hotmail.com>
2024-02-07 07:05:27 -0300

    Dia 1

---
+++ SiteDeRol/Controllers/MembroController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SiteDeRol.Controllers
+{
+    public class MembroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
