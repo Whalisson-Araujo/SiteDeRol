85b2b8c15dacaf2bf39010c4eed4014e3b8b3e9f
whalissonaraujo <139155812+Whalisson-Araujo@users.noreply.github.com>
2024-02-10 16:37:29 -0300

    Delete SiteDeRol directory

--- SiteDeRol/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SiteDeRol.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
